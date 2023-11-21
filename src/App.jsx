3da6dacb53398b888a161ac954aea435b6e8eb8d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-22 01:11:45 +0530

    Completed with rating of movie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
 import Movie from "./components/Movie/Movie";
 
 const App = () => {
-  const styles = {
-    cardCenter: "grid place-content-center border",
-  };
-
   return (
-    <>
-      <Navbar />
-      <div className="grid h-auto place-content-center gap-10 border px-10 py-10 sm:grid-cols-2 md:grid-cols-3">
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
-        <div className={styles.cardCenter}>
-          <Movie />
-        </div>
+    <main className="mx-auto max-w-[1660px]">
+      <div className="grid h-auto place-content-center gap-10 border px-10 py-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {/* {Array(15).fill(<Movie />)} */}
+        {Array(15)
+          .fill(true)
+          .map((item, index) => (
+            <Movie key={index} />
+          ))}
       </div>
-    </>
+    </main>
   );
 };
 
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { AiOutlineStar } from "react-icons/ai";
+
+function RatingStar() {
+  const stars = 4.2;
+  const ratingStar = Array.from({ length: 5 }, (element, index) => {
+    let number = index + 0.5;
+
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <FaStar className="text-base text-yellow-500" />
+        ) : stars >= number ? (
+          <FaStarHalfAlt className="text-base text-yellow-500" />
+        ) : (
+          <AiOutlineStar className="text-base text-yellow-500" />
+        )}
+      </span>
+    );
+  });
+
+  return (
+    <div className="mt-2 flex items-center justify-center">{ratingStar}</div>
+  );
+}
 
 const Movie = () => {
   return (
-    <div className="h-[500px] w-[250px] border border-purple-500">
-      <div className="">
-        <img
-          src="https://dummyimage.com/500x500.png?text=Movie%20Poster"
-          alt=""
-          srcset=""
-        />
+    <div className="grid place-content-center">
+      <div className="h-[450px] w-[250px]">
+        <div className="overflow-hidden rounded-2xl bg-contain">
+          <img src="./black-panther-web.jpg" alt="Movie Poster" />
+        </div>
+        <div className="h-auto">
+          <p className="balance mt-4 w-full overflow-hidden overflow-ellipsis whitespace-nowrap px-4 text-white">
+            Black Panther: Long Live The King
+          </p>
+          {RatingStar()}
+        </div>
       </div>
     </div>
   );
