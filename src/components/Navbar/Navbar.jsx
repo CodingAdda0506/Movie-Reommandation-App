a83bba8f2447bd9c0bf9a2b9a795e66a9efd4df0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-21 17:14:35 +0530

    completed with Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoMdSearch } from "react-icons/io";
 
 const Navbar = () => {
   const value = [
@@ -25,15 +26,21 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="text-white text-2xl bg-[#227fb4] flex justify-between items-center px-10 py-5">
+    <div className="flex items-center justify-between bg-[#227fb4] px-5 py-5 text-2xl text-white md:px-10">
       {/* <img src="" alt="" srcset=" " /> */}
       <p className="font-bold">MoviesAdda</p>
-      <input
-        type="text"
-        placeholder="Search..."
-        className="px-5 py-3 bg-[#23262e] rounded-lg outline-none text-[#9ca3af] text-base w-[280px]"
-      />
-      <select className="text-black w-[130px] bg-[#23262e] text-[#9ca3af] px-5 py-2 rounded-lg outline-none">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="hidden w-[200px] rounded-lg bg-[#23262e] py-2 pl-5 pr-10 text-base text-[#9ca3af] outline-none sm:block md:w-[280px]"
+        />
+        <IoMdSearch className="absolute right-2 top-1/2 hidden -translate-y-1/2 sm:block" />
+      </div>
+      <select
+        value="hari"
+        className="w-[130px] rounded-lg bg-[#23262e] px-5 py-2 text-base text-[#9ca3af] outline-none"
+      >
         <p>Select a Genre</p>
         {value.map((val) => (
           <option value={val}>{val}</option>
