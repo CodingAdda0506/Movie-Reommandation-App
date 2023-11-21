ceb3c8ce10aaee9375081b74a54ad56d496091f1
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-21 21:16:52 +0530

    Added a grid to movies page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Movie from "./components/Movie/Movie";
 
 const App = () => {
+  const styles = {
+    cardCenter: "grid place-content-center border",
+  };
+
   return (
     <>
       <Navbar />
+      <div className="grid h-auto place-content-center gap-10 border px-10 py-10 sm:grid-cols-2 md:grid-cols-3">
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+        <div className={styles.cardCenter}>
+          <Movie />
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Movie = () => {
-  return <div>Movie</div>;
+  return (
+    <div className="h-[500px] w-[250px] border border-purple-500">
+      <div className="">
+        <img
+          src="https://dummyimage.com/500x500.png?text=Movie%20Poster"
+          alt=""
+          srcset=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
