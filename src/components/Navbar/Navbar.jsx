c6c27aad42c2d2de00cee50e5d21314bef9de40e
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-21 21:50:47 +0530

    Change the responsiveness of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,26 +26,29 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="flex items-center justify-between bg-[#227fb4] px-5 py-5 text-2xl text-white md:px-10">
-      {/* <img src="" alt="" srcset=" " /> */}
-      <p className="font-bold">MoviesAdda</p>
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="hidden w-[200px] rounded-lg bg-[#23262e] py-2 pl-5 pr-10 text-base text-[#9ca3af] outline-none sm:block md:w-[280px]"
-        />
-        <IoMdSearch className="absolute right-2 top-1/2 hidden -translate-y-1/2 sm:block" />
+    <div className="mx-auto flex items-center justify-center border bg-[#227fb4] px-5 py-5">
+      <div className="flex w-full max-w-[1660px] items-center justify-between border border-red-500 text-2xl text-white md:px-10">
+        {/* <img src="" alt="" srcset=" " /> */}
+        <p className="font-bold">MoviesAdda</p>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="hidden w-[200px] rounded-lg bg-[#23262e] py-2 pl-5 pr-10 text-base text-[#9ca3af] outline-none sm:block md:w-[280px]"
+          />
+          <IoMdSearch className="absolute right-2 top-1/2 hidden -translate-y-1/2 sm:block" />
+        </div>
+        <select
+          value="hari"
+          className="w-[130px] rounded-lg bg-[#23262e] px-5 py-2 text-base text-[#9ca3af] outline-none"
+        >
+          {value.map((val, index) => (
+            <option key={index} value={val}>
+              {val}
+            </option>
+          ))}
+        </select>
       </div>
-      <select
-        value="hari"
-        className="w-[130px] rounded-lg bg-[#23262e] px-5 py-2 text-base text-[#9ca3af] outline-none"
-      >
-        <p>Select a Genre</p>
-        {value.map((val) => (
-          <option value={val}>{val}</option>
-        ))}
-      </select>
     </div>
   );
 };
