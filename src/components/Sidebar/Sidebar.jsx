427f4e3447fde6befcda71572698ae7673424faa
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-28 09:55:13 +0530

    Added Genre Images

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import { useGetGenresQuery } from '../../services/Api';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectGenre } from '../../Feature/currentGenre';
+import genreIcons from '../../assets/genres';
 
 const Sidebar = () => {
   const { data, isFetching } = useGetGenresQuery();
@@ -14,7 +15,10 @@ const Sidebar = () => {
       {isFetching || (
         data?.genres.map(({ name, id }) => (
           <Link key={id} value={id} className="text-white flex px-2 py-2 " onCl>
-            <div onClick={() => dispatch(selectGenre(id))}>
+            <div className='flex' onClick={() => dispatch(selectGenre(id))}>
+              <div>
+                <img src={genreIcons[name.toLowerCase()]} width={30} height={30} className='text-white invert mr-5'/>
+              </div>
               {name}
             </div>
           </Link>
