d33d47a4d4d237f68c91aec6bc8c6a229784776f
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-28 09:45:35 +0530

    Display Movie According to Genre

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGetGenresQuery } from '../../services/Api';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectGenre } from '../../Feature/currentGenre';
 
 const Sidebar = () => {
   const { data, isFetching } = useGetGenresQuery();
@@ -12,10 +13,10 @@ const Sidebar = () => {
     <div>
       {isFetching || (
         data?.genres.map(({ name, id }) => (
-          <Link key={id} value={id} className="text-white flex px-2 py-2 " >
-            {/* <div onClick={() => dispatch(selectGenre(genreName))}>
-            </div> */}
-            {name}
+          <Link key={id} value={id} className="text-white flex px-2 py-2 " onCl>
+            <div onClick={() => dispatch(selectGenre(id))}>
+              {name}
+            </div>
           </Link>
         ))
       )}
